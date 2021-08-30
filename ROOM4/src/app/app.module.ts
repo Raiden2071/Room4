4f06d9fd72aa44adae4090bab71632f58cac80d1
Raiden2071 <66531600+Raiden2071@users.noreply.github.com>
2021-08-30 12:00:18 +0300

    add main and login components

---
+++ ROOM4/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './modules/main/components/main/main.component';
+import { LoginComponent } from './modules/auth/components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
